d3db1047e6914699b6c6c36f9bb33d69cab867cf
BrankoSlavkov <slavkov.branko@gmail.com>
2022-12-05 23:54:56 +0100

    main styles added

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,19 @@
 import styles from './main.module.css';
 
 export const Main = () => {
-  return <div className={styles.container}>Main</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.questions__container}>
+        <p className={styles.question}>
+          On a radio, stations are changed by using what control?
+        </p>
+        <div className={styles.answers__container}>
+          <button className={styles.answer}>Tuning</button>
+          <button className={styles.answer}>Volume</button>
+          <button className={styles.answer}>Bass</button>
+          <button className={styles.answer}>Treble</button>
+        </div>
+      </div>
+    </div>
+  );
 };
