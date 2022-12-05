a0c0bb786a7a61bdecbd51510f87525c15265ca1
BrankoSlavkov <slavkov.branko@gmail.com>
2022-12-05 22:21:13 +0100

    aside styled

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,5 +1,24 @@
 import styles from './aside.module.css';
 
+const prices = [
+  100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000,
+  250000, 500000, 1000000,
+];
+
 export const Aside = () => {
-  return <div className={styles.container}>Aside</div>;
+  return (
+    <div className={styles.container}>
+      <ul className={styles.list}>
+        {prices.map((price, index) => (
+          // className={styles.active}
+          <li key={price}>
+            <span>{index + 1}</span>
+            <span className={styles.price}>
+              $ {price.toLocaleString('de-DE')}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
